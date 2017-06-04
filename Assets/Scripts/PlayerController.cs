7bd51104f3733253759bbf4fb37109ecaf8a6f01
MichaelCoyne1996 <15181847@studentmail.ul.ie>
2017-06-04 21:53:15 +0100

    moving

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -15,12 +15,22 @@ public class PlayerController : MonoBehaviour {
         controller = GetComponent<CharacterController>();
 		
 	}
-	
-	// Update is called once per frame
-	void Update () {
-        Vector3 input = new Vector3(Input.GetAxisRaw("Horizontal"), 0, Input.GetAxisRaw("Virtical"));
-        if(input != Vector3.zero)
-        targetRotation = Quaternion.LookRotation(input);
-        transform.eulerAngles = Vector3.up * Mathf.MoveTowardsAngle(transform.eulerAngles.y, targetRotation.eulerAngles.y, rotationSpeed * Time.deltaTime);
-	}
+
+    // Update is called once per frame
+    void Update()
+    {
+        Vector3 input = new Vector3(Input.GetAxisRaw("Horizontal"), 0, Input.GetAxisRaw("Vertical"));
+        if (input != Vector3.zero)
+        {
+            targetRotation = Quaternion.LookRotation(input);
+            transform.eulerAngles = Vector3.up * Mathf.MoveTowardsAngle(transform.eulerAngles.y, targetRotation.eulerAngles.y, rotationSpeed * Time.deltaTime);
+
+        }
+        Vector3 motion = input;
+        input *= (Mathf.Abs(input.x) == 1 && Mathf.Abs(input.z) == 1) ? .7f:1;
+        input *= (Input.GetButton("Run")) ? runSpeed : walkSpeed;
+        motion += Vector3.up * -8;
+
+        controller.Move(motion * Time.deltaTime);
+    }
 }
